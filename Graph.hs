992371a37dabbb01b76fbc36c12ed4c8b0cdd995
hask <hask@treblih.localdomain>
2010-07-31 04:45:56 +0800

    Graph adjacent matrix

--- Graph.hs
+++ Graph.hs
@@ -3,15 +3,15 @@ module Graph (
   create,
   adjacent,
   nodes,
-  edges_ndrct,
-  edges_drct,
+  weight,
   edge_in,
-  weight
+  edges_ndrct,
+  edges_drct
 ) where
 
 import Array
 
-{- 
+{- pointer implementation
   drawbacks:
 	1. not connected, each nodes generated separately
 	2. no explicit val, can't be manipulated & compared directly
@@ -22,15 +22,15 @@ data Graph n w = Vertex n [((Graph n w), w)]
 -}
 
 create :: (Ix n, Num w) => Bool -> (n, n) -> [(n, n, w)] -> Graph n w
--- figure out all adjacent vertices
+  -- figure out all adjacent vertices
 adjacent :: (Ix n, Num w) => Graph n w -> n -> [n]
--- how many vertices
+  -- how many vertices
 nodes :: (Ix n, Num w) => Graph n w -> [n]
+weight :: (Ix n, Num w, Show n) => Graph n w -> n -> n -> w
+  -- does 2 nodes have a edge
+edge_in :: (Ix n, Num w) => Graph n w -> n -> n -> Bool
 edges_ndrct :: (Ix n, Num w) => Graph n w -> [(n, n, w)]
 edges_drct :: (Ix n, Num w) => Graph n w -> [(n, n, w)]
--- does 2 nodes have a edge
-edge_in :: (Ix n, Num w) => Graph n w -> n -> n -> Bool
-weight :: (Ix n, Num w, Show n) => Graph n w -> n -> n -> w
 
 {- adjacent list implementation
 type Graph n w = Array n [(n, w)]
@@ -45,15 +45,42 @@ adjacent g x = map fst (g ! x)
 
 nodes g = indices g
 
-edge_in g x y = elem x $ adjacent g y
-
 weight g x y
   | edge_in g x y = head [w | (n, w) <- g ! x, n == y]
   | otherwise = error (show x ++ " and " ++ show y ++ " are not adjacent")
+
+edge_in g x y = elem x $ adjacent g y
 
 edges_ndrct g = [(x1, x2, w) | x1 <- nodes g, (x2, w) <- g ! x1, x1 < x2]
 
 edges_drct g = [(x1, x2, w) | x1 <- nodes g, (x2, w) <- g ! x1]
 -}
 
--- adjacent matrix implementation
+  -- adjacent matrix implementation
+type Graph n w = Array (n, n) (Maybe w)
+
+create dir bnd@(l, u) xs =
+  matrix_empty // ([((x, y), Just w) | (x, y, w) <- xs] ++
+                   if dir
+		      then []
+		      else [((y, x), Just w) | (x, y, w) <- xs, x /= y])
+    where
+    matrix_empty = array ((l, l), (u, u)) 
+	           [((x1, x2), Nothing) | x1 <- range bnd, x2 <- range bnd]
+
+adjacent g x = [y | y <- nodes g, g ! (x, y) /= Nothing]
+
+nodes g = range (l, u) where ((l, _), (u, _)) = bounds g
+
+weight g x y = w where Just w = g ! (x, y)
+
+edge_in g x y = g ! (x, y) /= Nothing
+
+edges_ndrct g = [(x, y, unwrap $ g ! (x, y)) |
+                  x <- nodes g, y <- nodes g, edge_in g x y]
+
+edges_drct g = [(x, y, unwrap $ g ! (x, y)) | 
+                 x <- nodes g, y <- nodes g, edge_in g x y, x < y]
+
+unwrap :: Maybe a -> a
+unwrap (Just a) = a
